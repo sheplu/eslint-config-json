d48a8ad8c2604adeda59ada0c00afa18edc12257
Jean Burellier <jean.burellier@gmail.com>
2025-12-01 23:16:23 +0100

    feat: add eslint-json config

---
+++ eslint-json.js
@@ -0,0 +1,31 @@
+export default [
+    {
+        rules: {
+            "json/no-duplicate-keys": [
+                "error",
+            ],
+            "json/no-empty-keys": [
+                "error",
+            ],
+            "json/no-unnormalized-keys": [
+                "error", {
+                    "form": "NFC",
+                },
+            ],
+            "json/no-unsafe-values": [
+                "error",
+            ],
+            "json/sort-keys": [
+                "error", "asc", {
+                    "caseSensitive": true,
+                    "natural": true,
+                    "minKeys": 2,
+                    "allowLineSeparatedGroups": false,
+                },
+            ],
+            "json/top-level-interop": [
+                "error",
+            ],
+        },
+    },
+];
