1d4cce7184aecf25ed58555510a3d991b84d2b22
Jean Burellier <jean.burellier@gmail.com>
2026-04-24 15:19:20 +0200

    feat: add types for linter

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import type { Linter } from 'eslint';
+
+export const jsonRules: Linter.Config[];
+export const defaultJsonConfig: Linter.Config;
+export const defaultJsoncConfig: Linter.Config;
+export const defaultJson5Config: Linter.Config;
